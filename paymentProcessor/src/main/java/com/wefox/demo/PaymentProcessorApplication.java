94aeb564afc2c4d158bf98f3982af5f07cc35bb0
Alejandro Moneo <amoneo@MacBook-Pro-de-Alejandro.local>
2021-09-29 16:28:38 +0200

    jpa config and schema validation

--- paymentProcessor/src/main/java/com/wefox/demo/PaymentProcessorApplication.java
+++ paymentProcessor/src/main/java/com/wefox/demo/PaymentProcessorApplication.java
@@ -2,9 +2,13 @@ package com.wefox.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.wefox.domain.entity")
+@EnableJpaRepositories(basePackages = {"com.wefox.repository.jpaRepository"})
 public class PaymentProcessorApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
